53b528a22b27fea513028258743cf3521ce2abda
thiago.tgn <tgermanon@gmail.com>
2024-01-27 09:23:09 -0300

    update header

--- src/components/HeaderG/index.jsx
+++ src/components/HeaderG/index.jsx
@@ -1,43 +1,61 @@
-import React from 'react';
-import { Container, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import './styles.css';
 
 function OffcanvasExample() {
   return (
-    <>
-      {[false].map((expand) => (
-        <Navbar  expand={expand} className="bg-body-tertiary mb-3">
+    <header className='Header-global'>
+
+       <Navbar key={'xxl'}  expand={'xxl'} fixed="top" className="NavBar-menu">
           <Container fluid>
-            <Navbar.Brand href="#"></Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Brand href="#" className="Canvas-menu">Projeto ENEM</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-xxl`} />
             <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
+              id={`offcanvasNavbar-expand-xxl`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-xxl`}
+              placement="start"
             >
               <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-xxl`}>
                   Menu
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Calculadora de Notas</Nav.Link>
-                  <Nav.Link href="#action2">Provas Antigas</Nav.Link>
-                  <Nav.Link href="#action2">Cronometro</Nav.Link>
-
-                  <NavDropdown title="Redação" id={`offcanvasNavbarDropdown-expand-${expand}`}>
-      <NavDropdown.Item href="">Dicas</NavDropdown.Item>
-      <NavDropdown.Item href="#opcao2">Nota Mil</NavDropdown.Item>
-      <NavDropdown.Item href="#opcao3">Folha em PDF</NavDropdown.Item>
-    </NavDropdown>
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/ProvasAntigas">Provas Antigas</Nav.Link>
+                  <Nav.Link href="/Calculadora">Calculadora de Notas</Nav.Link>
+                  <Nav.Link href="/Cronometro">Cronometro</Nav.Link>
+                  <Nav.Item  href="#action2">
+                    <Button>Login</Button>
+                    
+                    <Button>Cadastro</Button>
+                  </Nav.Item >
+                  <NavDropdown
+                    title="Redação"
+                    id={`offcanvasNavbarDropdown-expand-xxl`}
+                  >
+                    <NavDropdown.Item href="/Dicas">Dicas</NavDropdown.Item>
+                    <NavDropdown.Item href="/NotaMil">
+                      Nota Mil
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Folha em PDF
+                    </NavDropdown.Item>
+                  </NavDropdown>
                 </Nav>
+                
               </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
         </Navbar>
-      ))}
-    </>
+      </header>
   );
 }
 
