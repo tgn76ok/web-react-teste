9e74888d74b96c2c8a016a553899565154069613
Lígia kaylanne <142066909+ligiakaylanne@users.noreply.github.com>
2024-01-27 21:29:46 -0300

    atualização

--- src/components/HeaderG/index.jsx
+++ src/components/HeaderG/index.jsx
@@ -7,68 +7,49 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import './styles.css';
 
-
 function OffcanvasExample() {
   return (
     <header className='Header-global'>
-
-       <Navbar key={'xxl'}  expand={'xxl'} fixed="top" className="NavBar-menu">
-          <Container fluid>
-            <Navbar.Brand href="#" className="Canvas-menu">Projeto ENEM</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-xxl`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-xxl`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-xxl`}
-              placement="start"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-xxl`}>
-                  Menu
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-<<<<<<< HEAD
-                  <Nav.Link href="">Calculadora de Notas</Nav.Link>
-                  <Nav.Link href="#action2">Provas Antigas</Nav.Link>
-                  <Nav.Link href="#cronometro">Cronometro</Nav.Link>
-
-                  <NavDropdown title="Redação" id={`offcanvasNavbarDropdown-expand-${expand}`}>
-      <NavDropdown.Item href="">Dicas</NavDropdown.Item>
-      <NavDropdown.Item href="#opcao2">Nota Mil</NavDropdown.Item>
-      <NavDropdown.Item href="#opcao3">Folha em PDF</NavDropdown.Item>
-    </NavDropdown>
-=======
-                  <Nav.Link href="/">Home</Nav.Link>
-                  <Nav.Link href="/ProvasAntigas">Provas Antigas</Nav.Link>
-                  <Nav.Link href="/Calculadora">Calculadora de Notas</Nav.Link>
-                  <Nav.Link href="/Cronometro">Cronometro</Nav.Link>
-                  <Nav.Item  href="#action2">
-                    <Button>Login</Button>
-                    
-                    <Button>Cadastro</Button>
-                  </Nav.Item >
-                  <NavDropdown
-                    title="Redação"
-                    id={`offcanvasNavbarDropdown-expand-xxl`}
-                  >
-                    <NavDropdown.Item href="/Dicas">Dicas</NavDropdown.Item>
-                    <NavDropdown.Item href="/NotaMil">
-                      Nota Mil
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Folha em PDF
-                    </NavDropdown.Item>
-                  </NavDropdown>
->>>>>>> 53b528a22b27fea513028258743cf3521ce2abda
-                </Nav>
-                
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      </header>
+      <Navbar key={'xxl'} expand={'xxl'} fixed="top" className="NavBar-menu">
+        <Container fluid>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-xxl`} />
+          <Navbar.Brand href="#" className="Canvas-menu"></Navbar.Brand>
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-xxl`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-xxl`}
+            className="offcanvas-right" // Adicionando a classe personalizada
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-xxl`}>
+                Menu
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="/ProvasAntigas">Provas Antigas</Nav.Link>
+                <Nav.Link href="/Calculadora">Calculadora de Notas</Nav.Link>
+                <Nav.Link href="/Cronometro">Cronometro</Nav.Link>
+                <Nav.Item  href="#action2">
+                </Nav.Item >
+                <NavDropdown
+                  title="Redação"
+                  id={`offcanvasNavbarDropdown-expand-xxl`}
+                >
+                  <NavDropdown.Item href="/Dicas">Dicas</NavDropdown.Item>
+                  <NavDropdown.Item href="/NotaMil">
+                    Nota Mil
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Folha em PDF
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
 
--- src/templates/Calculadora/index.js
+++ src/templates/Calculadora/index.js
@@ -1,39 +1,72 @@
 import './App.css';
 
-
 function App() {
   return (
     <div className="main-notas">
-
-      <div class="notas">
+      <div className="notas">
         <h1>Simule sua nota:</h1>
         <form id="notaForm">
-            <label for="Nota1" className='Label-notas'>Sua nota em Linguagens, Códigos e suas Tecnologias:</label>
-            <input type="number" name="Nota1" className='input-notas'/>
+          <label htmlFor="Nota1" className='Label-notas'>
+            Sua nota em Linguagens, Códigos e suas Tecnologias:
+          </label>
+          <input
+            type="number"
+            name="Nota1"
+            className='input-notas'
+            maxLength="5"
+          />
 
-            <label for="Nota2" className='Label-notas'>Sua nota em Ciências da Natureza:</label>
-            <input type="number" name="Nota2" className='input-notas'/>
+          <label htmlFor="Nota2" className='Label-notas'>
+            Sua nota em Ciências da Natureza:
+          </label>
+          <input
+            type="number"
+            name="Nota2"
+            className='input-notas'
+            maxLength="5"
+          />
 
-            <label for="Nota3" className='Label-notas'>Sua nota em Ciências Humanas:</label>
-            <input type="number" name="Nota3" className='input-notas'/>
+          <label htmlFor="Nota3" className='Label-notas'>
+            Sua nota em Ciências Humanas:
+          </label>
+          <input
+            type="number"
+            name="Nota3"
+            className='input-notas'
+            maxLength="5"
+          />
 
-            <label for="Nota4" className='Label-notas'>Matemática e suas tecnologias:</label>
-            <input type="number" name="Nota4" className='input-notas'/>
+          <label htmlFor="Nota4" className='Label-notas'>
+            Matemática e suas tecnologias:
+          </label>
+          <input
+            type="number"
+            name="Nota4"
+            className='input-notas'
+            maxLength="5"
+          />
 
-            <label for="Nota5" className='Label-notas'> Sua nota em Redação:</label>
+          <label htmlFor="Nota5" className='Label-notas'>
+            Sua nota em Redação:
+          </label>
+          <input
+            type="number"
+            name="Nota5"
+            className='input-notas'
+            maxLength="5"
+          />
 
-            <input type="number" name="Nota5" className='input-notas'/>
-            
-            
-            <button type="button" className="Button-notas" id="calcularButton">Calcular Nota</button>
-            
-            <div id="resultado" class="resultado"></div>
-
+          <button
+            type="button"
+            className="Button-notas"
+            id="calcularButton"
+          >
+            Calcular Nota
+          </button>
+          <div id="resultado" className="resultado"></div>
         </form>
-        
+      </div>
     </div>
-    </div>
-
   );
 }
 
